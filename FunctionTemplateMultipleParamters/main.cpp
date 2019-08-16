34705782e87679679314fdb4798b8117165fb11f
Karthik Ram <karthikram570@gmail.com>
2019-08-16 11:48:45 +0530

    Example for function template multiple parameters

---
+++ FunctionTemplateMultipleParamters/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+template <class FIRST, class SECOND>
+
+FIRST smaller(FIRST a,SECOND b){
+    return (a<b?a:b);
+}
+
+int main()
+{
+    int x = 89;
+    double y = 56.68;
+    cout << smaller(x,y) << endl;
+    return 0;
+}
